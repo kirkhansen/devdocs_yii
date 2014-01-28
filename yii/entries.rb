064cbdbe9fb7d3cfc793a29a202fa0c29caf93fc
Kirk Hansen <kirk.hansen@colorfx.net>
2014-01-28 17:07:07 -0600

    Added get_name and get_type. Still not working as expected, however.

--- yii/entries.rb
+++ yii/entries.rb
@@ -1,7 +1,19 @@
 module Docs
   class Yii
     class EntriesFilter < Docs::EntriesFilter
-    
+     
+      def get_name
+        #class names exist in the <h1> content.
+        name = at_css('h1').content.strip
+        name
+      end
+
+      def get_type
+        #need to get the table with a class of summaryTable. Then we need the content of the first td in the first tr.
+	    type = css('table.summaryTable td')[0].content
+	    type
+		
+      end
     end
   end
 end
