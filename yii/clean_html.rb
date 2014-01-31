21ff2fd7ed64f7dcb66d2bdc53f601d017d63385
Kirk Hansen <kirk.hansen@colorfx.net>
2014-01-31 10:26:30 -0600

    added comments and removed some more content

--- yii/clean_html.rb
+++ yii/clean_html.rb
@@ -2,7 +2,8 @@ module Docs
   class Yii
     class CleanHtmlFilter < Filter
       def call
-
+		
+		#remove meaningless content
       	css('div').each do |node| 
 		  if node['class'] == "layout-main-header"
 			node.remove
@@ -18,13 +19,33 @@ module Docs
 			node.remove
 		  elsif node['class'] == "api-suggest clearfix"
 			node.remove
+		  elsif node['id'] == "comments"
+		    node.remove
+		  elsif node['id'] == "nav"
+		    node.remove
 		  else
 			end
-		  end	
+		  end
+		  
+		#remove javascript show links
+		css('b').each do |node|
+		  if node['class'] == "show"
+		    node.remove
+		  end
+		end
+				  	
 		# Put code blocks in <pre> tags
         css('.code').each do |node|
           node.name = 'pre'
 	    end
+	    
+	    #remove "Hide inherited methods / properties
+	    css('a').each do |node|
+	      if node['class'] == 'toggle'
+	        node.remove
+	      end
+	    end
+	    
         doc
       end
     end
